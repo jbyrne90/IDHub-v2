a97ccbbc06752e3a84a9860a03e3d57af81c3e98
Justin Byrne <jbyrne1030@gmail.com>
2018-07-16 09:37:11 +1000

    Initial commit of duplicated files from map-aus

---
+++ pins-config.js
@@ -0,0 +1,23 @@
+﻿var pins_config = {
+	'default':{
+		'pinShadow':'#000', //shadow color below the points
+		'pinShadowOpacity':'50', //shadow opacity, value, 0-100
+	},
+	'points':[
+	{
+		'shape':'circle',
+		'hover': '',
+		'pos_X':219,
+		'pos_Y':168,
+		'diameter':12,
+		'outline':'#FFF',
+		'thickness':1,
+		'upColor':'#592f62',
+		'overColor':'#592f62',
+		'downColor':'#592f62',
+		'url':'#',
+		'target':'none',
+		'enable':true,
+	}
+	]
+}
